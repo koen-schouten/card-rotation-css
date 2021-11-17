0c70440ad91766556b16862efa7f1d1616623b26
Koen Schouten <schouten_koen@hotmail.com>
2021-11-17 16:19:55 +0100

    Adding animationStart and end eventlisteners.

--- common/js/app.js
+++ common/js/app.js
@@ -5,14 +5,34 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function init(){
-    let cardElement = document.getElementById("card")
-    cardElement.addEventListener("click", (e)=>{
-        if(cardElement.classList.contains("back")){
-            cardElement.classList.add("front");
-            cardElement.classList.remove("back");
-        }else{
-            cardElement.classList.add("back");
-            cardElement.classList.remove("front");
+    const cardElement = document.getElementById("card")
+    const clickListener = (cardElement =>{
+        return ()=>{
+            if(cardElement.classList.contains("back")){
+                cardElement.classList.add("front");
+                cardElement.classList.remove("back");
+            }else{
+                cardElement.classList.add("back");
+                cardElement.classList.remove("front");
+            }
         }
-    })
+    })(cardElement);
+
+    addClickEventListenerToElement(cardElement, clickListener);
+
+    cardElement.addEventListener('animationstart', e=>{
+        removeClickEventListenerFromElement(cardElement, clickListener);
+    });
+    cardElement.addEventListener('animationend', e=>{
+
+        addClickEventListenerToElement(cardElement, clickListener);
+    });
+}
+
+function addClickEventListenerToElement(el, listener){
+    el.addEventListener("click", listener);
+}
+
+function removeClickEventListenerFromElement(el, listener){
+    el.removeEventListener("click", listener);
 }
